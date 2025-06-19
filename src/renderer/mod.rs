0b6ba5d4d8076f12f560342930a5129ee3845cfd
Creamsicle42 <defuego424242@gmail.com>
2025-06-19 12:16:13 -0700

    Addeed matrix transform for ui drawing

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,4 +1,4 @@
-use glium::{CapabilitiesSource, implement_vertex};
+use glium::{CapabilitiesSource, implement_vertex, uniform};
 use std::{collections::HashMap, sync::Arc};
 
 use eyre::Result;
@@ -18,10 +18,12 @@ const VERTEX_SHADER_SRC: &str = r#"
     layout (location = 0) in vec3 position;
     layout (location = 1) in vec3 color;
 
+    uniform mat4 matrix;
+
     smooth out vec3 vertex_color;
 
     void main() {
-        gl_Position = vec4(position, 1.0);
+        gl_Position = matrix * vec4(position, 1.0);
         vertex_color = color;
     }
 "#;
@@ -112,7 +114,7 @@ impl RenderState {
         self.ticks += 1;
         self.vertex_buffer.write(&[
             Vertex {
-                position: [0.0, 0.0 + (0.01 * self.ticks as f32).cos(), 0.0],
+                position: [0.0, 0.5 + (0.01 * self.ticks as f32).cos() * 0.5, 0.0],
                 color: [0.0, 0.0, 1.0],
             },
             Vertex {
@@ -125,14 +127,24 @@ impl RenderState {
             },
         ]);
 
+        let uniforms = uniform! {
+            matrix: [
+                [2.0, 0.0, 0.0, 0.0],
+                [0.0, -2.0, 0.0, 0.0],
+                [0.0, 0.0, 2.0, 0.0],
+                [-1.0, 1.0, 0.0, 1.0_f32],
+            ]
+        };
+
         let mut frame = self.device.draw();
+
         frame.clear_color(0.0, 0.0, 0.0, 0.0);
         frame
             .draw(
                 &self.vertex_buffer,
                 &indices,
                 &self.program,
-                &glium::uniforms::EmptyUniforms,
+                &uniforms,
                 &Default::default(),
             )
             .unwrap();
